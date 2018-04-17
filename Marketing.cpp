5c150e6dfadb1834ba0d0ec851acfa66591c96c0
Md Jubaer Hossain <jubaer1660@gmail.com>
2018-04-17 21:43:36 +0600

    Create Marketing.cpp

---
+++ Marketing.cpp
@@ -0,0 +1,5 @@
+The New Micro Catalogue:
+https://micro.bynder.com/media/?mediaId=7CB07F28-CA7A-407D-A6B86CC79CB5FAD4
+https://www.yumpu.com/en/document/fullscreen/56771470/micro-catalogue-2018-english​
+The New Micro Logos (Branding):
+https://micro.bynder.com/web/1793f49d62cf41ca/corporate-design-2017-18/
